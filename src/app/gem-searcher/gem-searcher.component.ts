31048420bc5fab81955b6658cf353425344411f2
kuzinalex <lyoxakuzin0@gmail.com>
2022-11-06 15:34:25 +0300

    added css files, refactored gem-searcher.component.html, fixed http-service;

--- src/app/gem-searcher/gem-searcher.component.ts
+++ src/app/gem-searcher/gem-searcher.component.ts
@@ -1,6 +1,8 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MintModel} from "../model/MintModel";
 import { DOCUMENT } from '@angular/common';
+import {RestServiceService} from "../service/rest-service.service";
+import {empty} from "rxjs";
 
 @Component({
   selector: 'app-gem-searcher',
@@ -12,13 +14,23 @@ export class GemSearcherComponent implements OnInit {
   mints:MintModel[]=  [
     { id: 12, blockchain: "SOL", projectName: "DeGods", twitterFollowers: 3123, projectTwitterUrl: "https//ww", projectDiscordUrl: "urldisc", mintDate: "11-10-22", mintPrice: "0.33 SOL", projectImgUrl: "thttps img", totalSupply: 5555 },
     { id: 12, blockchain: "SOL", projectName: "Monkey", twitterFollowers: 3123, projectTwitterUrl: "https//ww", projectDiscordUrl: "urldisc", mintDate: "11-10-22", mintPrice: "0.33 SOL", projectImgUrl: "thttps img", totalSupply: 5555 },
-    { id: 12, blockchain: "SOL", projectName: "Cats", twitterFollowers: 3123, projectTwitterUrl: "https//ww", projectDiscordUrl: "urldisc", mintDate: "11-10-22", mintPrice: "0.33 SOL", projectImgUrl: "thttps img", totalSupply: 5555 }
+    { id: 12, blockchain: "SOL", projectName: "Cats", twitterFollowers: 3123, projectTwitterUrl: "https//ww", projectDiscordUrl: "urldisc", mintDate: "11-10-22", mintPrice: "0.33 SOL", projectImgUrl: "thttps img", totalSupply: 5555 },
+    { id: 12, blockchain: "SOL", projectName: "DeGods", twitterFollowers: 3123, projectTwitterUrl: "https//ww", projectDiscordUrl: "urldisc", mintDate: "11-10-22", mintPrice: "0.33 SOL", projectImgUrl: "thttps img", totalSupply: 5555 },
+    { id: 12, blockchain: "SOL", projectName: "Monkey", twitterFollowers: 3123, projectTwitterUrl: "https//ww", projectDiscordUrl: "urldisc", mintDate: "11-10-22", mintPrice: "0.33 SOL", projectImgUrl: "thttps img", totalSupply: 5555 },{ id: 12, blockchain: "SOL", projectName: "DeGods", twitterFollowers: 3123, projectTwitterUrl: "https//ww", projectDiscordUrl: "urldisc", mintDate: "11-10-22", mintPrice: "0.33 SOL", projectImgUrl: "thttps img", totalSupply: 5555 },
+    { id: 12, blockchain: "SOL", projectName: "Monkey", twitterFollowers: 3123, projectTwitterUrl: "https//ww", projectDiscordUrl: "urldisc", mintDate: "11-10-22", mintPrice: "0.33 SOL", projectImgUrl: "thttps img", totalSupply: 5555 },{ id: 12, blockchain: "SOL", projectName: "DeGods", twitterFollowers: 3123, projectTwitterUrl: "https//ww", projectDiscordUrl: "urldisc", mintDate: "11-10-22", mintPrice: "0.33 SOL", projectImgUrl: "thttps img", totalSupply: 5555 },
+    { id: 12, blockchain: "SOL", projectName: "Monkey", twitterFollowers: 3123, projectTwitterUrl: "https//ww", projectDiscordUrl: "urldisc", mintDate: "11-10-22", mintPrice: "0.33 SOL", projectImgUrl: "thttps img", totalSupply: 5555 },{ id: 12, blockchain: "SOL", projectName: "DeGods", twitterFollowers: 3123, projectTwitterUrl: "https//ww", projectDiscordUrl: "urldisc", mintDate: "11-10-22", mintPrice: "0.33 SOL", projectImgUrl: "thttps img", totalSupply: 5555 },
+    { id: 12, blockchain: "SOL", projectName: "Monkey", twitterFollowers: 3123, projectTwitterUrl: "https//ww", projectDiscordUrl: "urldisc", mintDate: "11-10-22", mintPrice: "0.33 SOL", projectImgUrl: "thttps img", totalSupply: 5555 },
   ];
 
+   data:Object=[];
 
-  constructor(@Inject(DOCUMENT) private document: Document) { }
+  constructor(@Inject(DOCUMENT) private document: Document, private service:RestServiceService) { }
 
   ngOnInit(): void {
+    this.service.get().subscribe(data=>{
+      this.data=data
+      console.log(data)
+    })
   }
 
   goToUrl(): void {
